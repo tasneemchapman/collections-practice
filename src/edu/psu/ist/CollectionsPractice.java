92741565d6f843257e0e4c956d1f7bd16e760d4e
tasneemchapman <145393906+tasneemchapman@users.noreply.github.com>
2024-10-07 19:36:22 -0400

    Update CollectionsPractice.java

--- src/edu/psu/ist/CollectionsPractice.java
+++ src/edu/psu/ist/CollectionsPractice.java
@@ -1,9 +1,36 @@
 package edu.psu.ist;
 
+import java.util.Stack;
+
 public final class CollectionsPractice {
 
     // note: this class should have no instance data/fields
     //       just do 1-4 using static methods (then test said
     //       methods from the 'test' directory using jUnit)
+    public static boolean balanced(String bracketStack ) {
+        Stack<Character> stackOfParenthesis = new Stack<>();
+        for (int i = 0; i < bracketStack.length(); i++) {
+            char ch = bracketStack.charAt(i);
+            if (ch== '(' || ch=='[') {
+                stackOfParenthesis.push(ch);
+            }
+            else if (ch==']') {
+                if (stackOfParenthesis.peek() == '[') {
+                    stackOfParenthesis.pop();
+                } else {
+                    return false;
+                }
+            }
+                else if (ch==')') {
+                    if (stackOfParenthesis.peek() == '(') {
+                        stackOfParenthesis.pop();
+                    }
+                    else {
+                        return false;
+                    }
+            }
+        }
+
+    }
 
 }
